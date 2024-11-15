9338e00fb543c0f2f8d2b2b67a56a80381d9e044
ĐẶNG NGỌC KHIÊU <dangngockhieu28082005@gmail.com>
2024-11-15 09:59:47 +0700

    CrossEntropy.cpp

--- Code/src/ann/loss/CrossEntropy.cpp
+++ Code/src/ann/loss/CrossEntropy.cpp
@@ -15,11 +15,7 @@ CrossEntropy::~CrossEntropy() {
 double CrossEntropy::forward(xt::xarray<double> X, xt::xarray<double> t){
     this->m_aYtarget = t;
     this->m_aCached_Ypred = X;  
-    const double EPSILON = 1e-7; 
-    auto x = xt::clip(X, EPSILON, 1.0 - EPSILON);  
-    
-    auto loss = -xt::sum(t * xt::log(x)) / X.shape()[0];
-    return loss();
+    return cross_entropy(X, t, REDUCE_MEAN);
 }
 xt::xarray<double> CrossEntropy::backward() {
     const double EPSILON = 1e-7;
